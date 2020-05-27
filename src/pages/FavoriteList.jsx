b4ff51fc313a4344d22feb6c81c997c986326aa4
Roxana Scutaru <rm.scutaru@gmail.com>
2020-05-27 23:08:53 +0300

    Favorite functionality with header for the message ref no favorite products

--- src/pages/FavoriteList.jsx
+++ src/pages/FavoriteList.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import FavoriteItem from '../components/FavoriteItem';
 import { connect } from 'react-redux';
+import Layout from '../components/Layout';
+import {Link} from 'react-router-dom';
 
 function FavoriteList(props) {
     const { favorites } = props;
 
     return (
+        <Layout>
         <div className="container-fluid container-min-max-width d-flex flex-column justify-content-center align-items-center">
         <div className="product-item col-12 col-md-4 mb-3 d-flex flex-row align-items-center justify-content-center ">
             {
@@ -18,10 +21,15 @@ function FavoriteList(props) {
                             />
                         </div>
                         })
-                    :<p className="ml-5 text-info"> No items added in favorite list! </p>
+                    :<div className="d-flex flex-column align-items-center">
+                    <p className="h3 text-center"> Nu ai produse favorite! </p>
+                    <Link to="/"><button className="btn btn-outline-dark">Inapoi la home</button></Link>
+                    </div>
             }
+
         </div>
         </div>
+        </Layout>
     );
 }
 
