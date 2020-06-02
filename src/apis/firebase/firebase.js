c13f7cb01f611619e12c518cd25c7490a406a172
Roxana Scutaru <rm.scutaru@gmail.com>
2020-06-02 10:27:59 +0300

    fixed gitignore for firebase

---
+++ src/apis/firebase/firebase.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from '../../configs/firebase';
+
+firebase.initializeApp(firebaseConfig);
+const googleProvider = new firebase.auth.GoogleAuthProvider()
+
+export const signOut = function() {
+    return firebase.auth().signOut();
+}
+
+export const signInWithGoogle = function() {
+    return firebase.auth().signInWithPopup(googleProvider);
+}
+
+const facebookProvider = new firebase.auth.FacebookAuthProvider();
+
+export const signInWithFacebook= function(){
+    return firebase.auth().signInWithPopup(facebookProvider);
+}
